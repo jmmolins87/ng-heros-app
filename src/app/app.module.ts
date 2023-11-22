859ffad17dbe3d919bd23fbe7717ddab10cb33d4
jmmcDesign <35576283+jmmolins87@users.noreply.github.com>
2023-11-22 21:28:01 +0100

    import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AuthModule } from './auth/auth.module';
+import { HeroesModule } from './heroes/heroes.module';
+import { MaterialModule } from './material/material.module';
+import { SharedModule } from './shared/shared.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule,
+    HeroesModule,
+    MaterialModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
