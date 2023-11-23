365e65f474c34cc02ddb59804f56734d39f3c58f
jmmcDesign <35576283+jmmolins87@users.noreply.github.com>
2023-11-23 18:31:35 +0100

    format code

--- src/app/heroes/pipes/heroImg.pipe.ts
+++ src/app/heroes/pipes/heroImg.pipe.ts
@@ -7,12 +7,8 @@ import { Hero } from '../interfaces/hero.interface';
 
 export class HeroImgPipe implements PipeTransform {
     transform( hero: Hero ): string {
-        if( !hero.id && !hero.alt_img ) {
-            return 'assets/no-image.png'
-        }
-
+        if( !hero.id && !hero.alt_img ) { return 'assets/no-image.png' }
         if( hero.alt_img ) return hero.alt_img;
-
         return `assets/heroes/${ hero.id }.jpg`
     }
 }
